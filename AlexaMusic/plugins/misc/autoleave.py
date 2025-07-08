aaf9f5608b7291ab04acecfbfda7d751bfac45f1
Blackmamba <122316561+FantasticSukhi@users.noreply.github.com>
2025-07-08 17:41:20 +0530

    Update autoleave.py

--- AlexaMusic/plugins/misc/autoleave.py
+++ AlexaMusic/plugins/misc/autoleave.py
@@ -1,14 +1,3 @@
-# Copyright (C) 2025 by Alexa_Help @ Github, < https://github.com/TheTeamAlexa >
-# Subscribe On YT < Jankari Ki Duniya >. All rights reserved. © Alexa © Yukki.
-
-"""
-TheTeamAlexa is a project of Telegram bots with variety of purposes.
-Copyright (c) 2021 ~ Present Team Alexa <https://github.com/TheTeamAlexa>
-
-This program is free software: you can redistribute it and can modify
-as you want or you can collabe if you have new ideas.
-"""
-
 import asyncio
 from datetime import datetime
 from pyrogram.enums import ChatType
@@ -44,7 +33,7 @@ async def auto_leave():
                         chat_id = i.chat.id
                         if chat_id not in [
                             config.LOG_GROUP_ID,
-                            -1001686672798,
+                            -1002124344872,
                         ] and not await is_active_chat(chat_id):
                             try:
                                 await client.leave_chat(chat_id)
